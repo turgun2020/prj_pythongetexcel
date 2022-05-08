e38139954249be68e05ebb731a9e55cb152e5b50
TurgunAzizi <turgun.azizi@gmail.com>
2022-05-08 21:34:40 +0900

    一回のコミット

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paydb.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300),  nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
