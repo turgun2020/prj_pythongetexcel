1b97f67441b43e811538aff6488d8afa31ab93bd
TurgunAzizi <turgun.azizi@gmail.com>
2022-05-08 21:49:02 +0900

    20220508

--- app.py
+++ app.py
@@ -8,12 +8,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paydb.db'
 db = SQLAlchemy(app)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    body = db.Column(db.String(300),  nullable=False)
+    body = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    return render_template('create.html')
